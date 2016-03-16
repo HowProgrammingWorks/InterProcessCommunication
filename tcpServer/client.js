700c05da7072c32cfae64fd10d042135fc1562f6
tshemsedinov <timur.shemsedinov@gmail.com>
2016-03-16 11:11:52 +0200

    Fixed tabs and added `api` namespace

--- tcpServer/client.js
+++ tcpServer/client.js
@@ -1,15 +1,18 @@
-var net = require('net');
-var socket = new net.Socket();
+var api = {};
+global.api = api;
+api.net = require('net');
+
+var socket = new api.net.Socket();
 var user;
 
 socket.connect({
-	port: 2000,
-	host: '127.0.0.1',
+  port: 2000,
+  host: '127.0.0.1',
 }, function() {
-	socket.write('Hello from client');
-	socket.on('data', function(data) {
-		user = JSON.parse(data);
-		console.log('Data received (by client): ' + data);
-		console.log('Age of ' + user.name + ' is ' + user.age);
-	});
+  socket.write('Hello from client');
+  socket.on('data', function(data) {
+    user = JSON.parse(data);
+    console.log('Data received (by client): ' + data);
+    console.log('Age of ' + user.name + ' is ' + user.age);
+  });
 });
--- tcpServer/server.js
+++ tcpServer/server.js
@@ -1,11 +1,13 @@
-var net = require('net');
+var api = {};
+global.api = api;
+api.net = require('net');
 
 var user = { name: 'Marcus Aurelius', age: 1895 };
 
-var server = net.createServer(function(socket) {
-	socket.write(JSON.stringify(user));
-	console.log('Connected: ' + socket.localAddress);
-	socket.on('data', function(data) {
-		console.log('Data received (by server): ' + data);
-	});
+var server = api.net.createServer(function(socket) {
+  socket.write(JSON.stringify(user));
+  console.log('Connected: ' + socket.localAddress);
+  socket.on('data', function(data) {
+    console.log('Data received (by server): ' + data);
+  });
 }).listen(2000);
