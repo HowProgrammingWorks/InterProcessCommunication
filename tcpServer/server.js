f5d204ab37022ef193bf4cecf9d86fe6d7397618
Romm17 <romanysik@gmail.com>
2016-05-20 21:02:11 +0300

    Some fix

--- tcpServer/server.js
+++ tcpServer/server.js
@@ -1,44 +1,52 @@
 var api = {};
 global.api = api;
 api.net = require('net');
+api.os = require('os');
 
-var contributors = 4;
+var contributorsCount = api.os.cpus().length;
+
+var contributors = [];
 
 var task = [2, 17, 3, 2, 5, 7, 15, 22, 1, 14, 15, 9, 0, 11];
 var results = [];
 
 function min(a, b) {
-return a > b ? b : a;
+	return a > b ? b : a;
 }
 
 var started = 0;
 var finished = 0;
 
-var taskSize = Math.ceil(task.length / contributors
-)
+var taskSize = Math.ceil(task.length / contributorsCount);
 console.log('Task size : ' + taskSize);
 
 var server = api.net.createServer(function(socket) {
 
-	if (started >= contributors) {
+	if (started >= contributorsCount) {
 		socket.write(JSON.stringify("Thank you, but no more contributors required"));
 	}
 	else {
-		var l = taskSize * started;
-	    var r = min(taskSize * (started + 1), task.length);
 
-	  socket.write(JSON.stringify({
-	  	id : started++,
-	  	data : task.slice(l, r)
-	  }));
+	  contributors[started++] = socket;
+	  if (started == contributorsCount) {
+	  	for (var i = 0; i < contributorsCount; i++) {
+	  		var l = taskSize * i;
+	    	var r = min(taskSize * (i + 1), task.length);
+	  		contributors[i].write(JSON.stringify({
+		  	id : i,
+		  	data : task.slice(l, r)
+		  }));
+	  	}
+	  }
+	  
 	  console.log('Connected: ' + socket.localAddress);
 	  socket.on('data', function(data) {
 	    console.log('Data received (by server): ' + data);
 	    obj = JSON.parse(data);
 	    results[obj.id] = obj.data;
 	    finished++;
-	    if (finished === contributors) {
-		    console.log(results);
+	    if (finished === contributorsCount) {
+		    console.log('results : ' + results);
 		}
 	  });
 	}
