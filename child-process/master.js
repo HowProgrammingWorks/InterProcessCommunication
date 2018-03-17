b48da80a862445a3049cb60f05415e4c0e3b9acc
tshemsedinov <timur.shemsedinov@gmail.com>
2018-03-17 06:17:09 +0200

    Add child-process example

---
+++ child-process/master.js
@@ -0,0 +1,42 @@
+'use strict';
+
+const os = require('os');
+const cp = require('child_process');
+
+console.log('Started master:', process.pid);
+
+const cpuCount = os.cpus().length;
+const workers = [];
+for (let i = 0; i < cpuCount; i++) {
+  const worker = cp.fork('./worker.js');
+  console.log('Started worker:', worker.pid);
+  workers.push(worker);
+}
+
+const task = [2, 17, 3, 2, 5, 7, 15, 22, 1, 14, 15, 9, 0, 11];
+const results = [];
+
+workers.forEach((worker) => {
+
+  worker.send({ task });
+
+  worker.on('exit', (code) => {
+    console.log('Worker exited:', worker.pid, code);
+  });
+
+  worker.on('message', (message) => {
+
+    console.log('Message from worker', worker.pid);
+    console.log(JSON.stringify(message));
+
+    results.push(message.result);
+
+    if (results.length === cpuCount) {
+      process.exit(0);
+    }
+
+  });
+
+  setTimeout(() => process.exit(1), 5000);
+
+});
---
+++ child-process/worker.js
@@ -0,0 +1,13 @@
+'use strict';
+
+console.log('Hello from worker', process.pid);
+
+const caltulations = item => item * 2;
+
+process.on('message', (message) => {
+  console.log('Message to worker', process.pid);
+  console.log('from master:', JSON.stringify(message));
+
+  const result = message.task.map(caltulations);
+  process.send({ result });
+});
