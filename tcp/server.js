f33c1d7f9c26cb7b0cf53a6b536f5a2f6d825db0
tshemsedinov <timur.shemsedinov@gmail.com>
2018-03-17 06:15:05 +0200

    Changes in TCP example

--- tcp/server.js
+++ tcp/server.js
@@ -5,10 +5,10 @@ const net = require('net');
 const user = { name: 'Marcus Aurelius', age: 1895 };
 
 const server = net.createServer((socket) => {
-  console.log('Connected: ' + socket.localAddress);
+  console.log('Connected:', socket.localAddress);
   socket.write(JSON.stringify(user));
   socket.on('data', (data) => {
-    console.log('Data received (by server): ' + data);
+    console.log('Data received (by server):', data);
   });
 });
 
