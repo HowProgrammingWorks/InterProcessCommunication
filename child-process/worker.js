080354955aaf807ad28d5bd86816dc91826cec2d
tshemsedinov <timur.shemsedinov@gmail.com>
2018-10-14 00:35:50 +0300

    Improve child_process examples readability

--- child-process/worker.js
+++ child-process/worker.js
@@ -4,9 +4,9 @@ console.log('Hello from worker', process.pid);
 
 const caltulations = item => item * 2;
 
-process.on('message', (message) => {
+process.on('message', message => {
   console.log('Message to worker', process.pid);
-  console.log('from master:', JSON.stringify(message));
+  console.log('from master:', message);
 
   const result = message.task.map(caltulations);
   process.send({ result });
